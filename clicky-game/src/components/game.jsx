2c12bafeba5bd420e0fdeb3669839b20bd0f8bdd
Jamie ONeill <joneill@sembracare.com>
2020-01-04 22:20:17 -0500

    Top Score is working

--- clicky-game/src/components/game.jsx
+++ clicky-game/src/components/game.jsx
@@ -1,34 +1,10 @@
 import React, { Component } from "react";
 import Header from "./header";
-import img1 from "../images/img1.jpg";
-import img2 from "../images/img2.jpg";
-import img3 from "../images/img3.jpg";
-import img4 from "../images/img4.jpg";
-import img5 from "../images/img5.jpg";
-import img6 from "../images/img6.jpg";
-import img7 from "../images/img7.jpg";
-import img8 from "../images/img8.jpg";
-import img9 from "../images/img9.jpg";
-import img10 from "../images/img10.jpg";
-import img11 from "../images/img11.jpg";
-import img12 from "../images/img12.jpg";
+import Sources from "../data";
 
 class Game extends Component {
   state = {
-    sources: [
-      img1,
-      img2,
-      img3,
-      img4,
-      img5,
-      img6,
-      img7,
-      img8,
-      img9,
-      img10,
-      img11,
-      img12
-    ],
+    Sources,
     score: 0,
     topScore: 0
   };
@@ -38,12 +14,18 @@ class Game extends Component {
     if (selectedImg.getAttribute("clicked") === "false") {
       selectedImg.setAttribute("clicked", "true");
       this.setState({ score: this.state.score + 1 });
-      shuffle();
+      if (this.state.score >= this.state.topScore) {
+        this.setState({ topScore: this.state.score + 1 });
+      }
     } else {
       console.log("GAME OVER");
       this.setState({ score: 0 });
-      console.log("Score when you lose " + this.state.score);
+      let reset = document.getElementsByClassName("img-fluid");
+      for (let i = 0; i < 12; i++) {
+        reset[i].setAttribute("clicked", false);
+      }
     }
+    shuffle();
   };
 
   render() {
@@ -57,7 +39,7 @@ class Game extends Component {
           <div className="row" style={{ "margin-top": 70 }}>
             <div className="col-12">
               <ul className="d-flex flex-wrap">
-                {this.state.sources.map((source, index) => (
+                {this.state.Sources.map((source, index) => (
                   <img
                     onClick={this.handleClick}
                     key={source}
