b51bc311445a7366b907e7dabc071de135a861b9
Jamie ONeill <joneill@sembracare.com>
2020-01-11 09:41:28 -0500

    Updates

---
+++ precache-manifest.359c6271818914eb96db47cb53c9b509.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02d8a2c2189c9ae5226d5d8d5d63a2b",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "6a9e70f101f32386245b",
+    "url": "/clickyGame/static/css/2.47e3df45.chunk.css"
+  },
+  {
+    "revision": "6a9e70f101f32386245b",
+    "url": "/clickyGame/static/js/2.48cfe3ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickyGame/static/js/2.48cfe3ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c21f133dd91ae48322b",
+    "url": "/clickyGame/static/js/main.bbf44b2a.chunk.js"
+  },
+  {
+    "revision": "f8e8d73f271021552fbf",
+    "url": "/clickyGame/static/js/runtime-main.54eabe31.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/clickyGame/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/clickyGame/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/clickyGame/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/clickyGame/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/clickyGame/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "330e00494ca12f3221d4190b1bfddec9",
+    "url": "/clickyGame/static/media/img1.330e0049.jpg"
+  }
+]);
